1640553eaebde576d824f98da36035a8b8dbcf0d
Andrey Nikitin <anikitin@codio.com>
2020-10-22 16:11:55 +0300

    do nothing on logo click

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -8,7 +8,7 @@ import HashParserHOC from '../lib/hash-parser-hoc.jsx';
 import log from '../lib/log.js';
 
 const onClickLogo = () => {
-    window.open('https://scratch.mit.edu', '_blank');
+    // do nothing
 };
 
 const handleTelemetryModalCancel = () => {
